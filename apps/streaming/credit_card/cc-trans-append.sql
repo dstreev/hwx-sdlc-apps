495d628a7eb900bdabd6758e735c67377dc67ca3
David W. Streever <david@streever.com>
2018-11-21 18:43:53 -0500

    topic change

--- apps/streaming/credit_card/cc-trans-append.sql
+++ apps/streaming/credit_card/cc-trans-append.sql
@@ -4,5 +4,5 @@ FROM CC_TRANS_BRIDGE
 SELECT
   CC_TRANS,CCN,TRANS_TS,MCC,MRCH_ID,STATE,AMNT
 WHERE
-  MINUTE_OF_DAY=${last.processing.cycle}
+  MINUTE_OF_DAY=${last.delta.processing.cycle.int}
 INSERT INTO TABLE CC_TRANS PARTITION ("${acid.processing.cycle.string}");
